7feea445816dffb434bce110941a2bfd55bb87b1
tarakeshwaran1 <101871375+tarakeshwaran1@users.noreply.github.com>
2022-04-11 08:57:50 +0530

    Create main.c

---
+++ 3_Implementation/main.c
@@ -0,0 +1,62 @@
+ #include"Train_ticket_reservation.h"
+ int main()
+
+{
+		system("cls");
+	printf("\t\t=================================================\n");
+	printf("\t\t|                                               |\n");
+	printf("\t\t|        -----------------------------          |\n");
+	printf("\t\t|           TRAIN TICKET RERS. SYSTEM           |\n");
+	printf("\t\t|        -----------------------------          |\n");
+	printf("\t\t|                                               |\n");
+	printf("\t\t|                                               |\n");
+	printf("\t\t|                                               |\n");
+	printf("\t\t|              BROUGHT TO YOU BY                |\n");
+	printf("\t\t|           |  code-projects.org  |             |\n");
+	printf("\t\t|                                               |\n");
+	printf("\t\t=================================================\n\n\n");
+
+
+	printf(" \n Press any key to continue:");
+
+	getch();
+    system("cls");
+	login();
+	int menu_choice,choice_return;
+	start:
+	system("cls");
+	printf("\n=================================\n");
+	printf("    TRAIN RESERVATION SYSTEM");
+	printf("\n=================================");
+	printf("\n1>> Reserve A Ticket");
+	printf("\n------------------------");
+	printf("\n2>> View All Available Trains");
+	printf("\n------------------------");
+	printf("\n3>> Cancel Reservation");
+	printf("\n------------------------");
+	printf("\n4>> Exit");
+	printf("\n------------------------");
+	printf("\n\n-->");
+	scanf("%d",&menu_choice);
+	switch(menu_choice)
+	{
+		case 1:
+			reservation();		//Fucntion still not added
+			break;
+		case 2:
+			viewdetails();
+			printf("\n\nPress any key to go to Main Menu..");
+			getch();
+			break;
+		case 3:
+			cancel();
+			//function not added. code has been removed due to some errors
+			break;
+		case 4:
+			return(0);
+		default:
+			printf("\nInvalid choice");
+	}
+	goto start;
+	return(0);
+}
